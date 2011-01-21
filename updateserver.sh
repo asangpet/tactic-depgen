3f855838b6149af987867f2b1586f240ad574a20
Akkarit Sangpetch <asangpet@andrew.cmu.edu>
2011-01-21 17:03:02 -0500

    Simplify server update (just use scp)

--- updateserver.sh
+++ updateserver.sh
@@ -2,5 +2,6 @@
 
 for i in {1..15}
 do
-  ssh root@node1v$i "hostname && cd /root/node && git stash && git pull"
+#  ssh root@node1v$i "hostname && cd /root/node && git stash && git pull"
+  scp server.js root@node1v$i:/root/node/server.js
 done
